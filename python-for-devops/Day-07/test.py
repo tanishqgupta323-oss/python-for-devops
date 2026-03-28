2c939e36c6b2d797b4a327de941bdb88b0232061
Tanishq_gupta <tanishqgupta323@gmail.com>
2026-03-28 09:20:43 +0530

    hey

---
+++ python-for-devops/Day-07/test.py
@@ -0,0 +1,11 @@
+import sys 
+type =sys.argv[1]
+if type == "t2.micro":
+    print("ok,we will create the instant for you")
+elif type == "t2.medium":
+    print("it will charge 4 dollar a day")
+elif type == "t2.large":
+    print("it will charge 8 dollar a day")
+
+else:
+    print("give a valid instance type, so we can not create the instant for you") 
